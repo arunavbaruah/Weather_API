ee03e49d957fbeddad9cfa0b5d3ccad51d94430c
Arunav Majinder Baruah <128210591+arunavbaruah@users.noreply.github.com>
2025-05-23 15:04:05 +0530

    Add files via upload

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "b2c71655b1793a73461eea028db546ab",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
